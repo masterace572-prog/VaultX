20fa5b475b100ba23670cd9a0c46bcd965f420a2
masterace572-prog <masterace572@gmail.com>
2026-06-14 03:17:34 +0530

    Increment admin version to 2.5.0 (versionCode 13)

--- vaultx-admin/build.gradle.kts
+++ vaultx-admin/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vaultx.admin"
         minSdk = 26
         targetSdk = 34
-        versionCode = 12
-        versionName = "2.4.0"
+        versionCode = 13
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
